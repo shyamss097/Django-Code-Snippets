eeb639feab9635e6a9618e73a328d7f5d882d5a9
Shyam Sundar S <shyamss097@gmail.com>
2023-02-23 22:14:50 +0530

    added proxy models with managers

--- accounts/models.py
+++ accounts/models.py
@@ -9,11 +9,27 @@ class CustomUser(AbstractUser):
 
     role = models.CharField(('Roles'), max_length=50, choices=Roles.choices, default=Roles.ROLE1)
     
+class Role1Manager(models.Manager):
+    def get_queryset(self,*args,**kwargs):
+        return super().get_queryset(*args,**kwargs).filter(role=CustomUser.Roles.ROLE1)
+
 class Role1(CustomUser):
+    objects = Role1Manager()
     class Meta:
         proxy = True
 
+    def save(self, *args, **kwargs):
+        if not self.pk:
+            self.role = CustomUser.Roles.ROLE1
+        return super().save(*args, **kwargs)
+
+
+class Role2Manager(models.Manager):
+    def get_queryset(self,*args,**kwargs):
+        return super().get_queryset(*args,**kwargs).filter(role=CustomUser.Roles.ROLE2)
 
 class Role2(CustomUser):
+    objects = Role2Manager()
     class Meta:
         proxy = True
+
